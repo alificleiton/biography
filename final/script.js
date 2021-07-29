3cf4057aa4243e261f1724d199aeeb38aac8d76d
Alifi Cleiton <alificleiton123@gmail.com>
2021-07-29 10:47:16 -0300

    adding new files

---
+++ final/script.js
@@ -0,0 +1,74 @@
+//javascript for navigation bar effects on scroll
+
+window.addEventListener("scroll", function(){
+  const header = document.querySelector("header");
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+
+//javascript of responsive navigation sidebar menu
+const menuBtn = document.querySelector(".menu-btn");
+const closeBtn = document.querySelector(".close-btn");
+const navigation = document.querySelector(".navigation");
+const navigationItems = document.querySelectorAll(".navigation a");
+
+menuBtn.addEventListener("click", () =>{
+  closeBtn.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+closeBtn.addEventListener("click", () =>{
+  closeBtn.classList.remove("active");
+  navigation.classList.toggle("active");
+});
+
+navigationItems.forEach((navigationItem) =>{
+  navigationItem.addEventListener("click", () =>{
+    menuBtn.classList.remove("active");
+    closeBtn.classList.remove("active");
+    navigation.classList.remove("active");
+  });
+});
+
+//javascript for scroll to top button
+const scrollBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+  scrollBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+
+//javascript for scroll back to top
+scrollBtn.addEventListener("click", () =>{
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+//javascript for reveal elements on scroll
+window.addEventListener("scroll", reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll(".reveal");
+
+  for( var i = 0; i < reveals.length; i++){
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 50;
+
+    if(revealTop < windowHeight - revealPoint){
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+//fazer um efeito de máquina de escrever no home
+const titulo = document.querySelector('.titulo');
+typeWritter(titulo);
+
+function typeWritter(elemento){
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML=' ';
+  textoArray.forEach((letra, i) => {
+    setTimeout(()=> elemento.innerHTML += letra, 75 * i)
+  });
+}
